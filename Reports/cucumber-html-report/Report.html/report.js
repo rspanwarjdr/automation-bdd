6147afa6a28e37df71dd7d538a336817a9e7f0bd
Raviendra <rspanwar@gmail.com>
2019-05-16 00:18:11 +0530

    Updating the code

---
+++ Reports/cucumber-html-report/Report.html/report.js
@@ -0,0 +1,423 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("OrderT-Shirt.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Order T-Shirt",
+  "description": "",
+  "id": "order-t-shirt",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Order and Validate Purchase",
+  "description": "",
+  "id": "order-t-shirt;order-and-validate-purchase",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@Test"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "Open browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User click on Sign In link",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User enter username \"automationpractice25@gmail.com\" and password \"test@1234\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "Click on SignIn button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User clicks on T-Shirts tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User add t-shirt in Cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "User clicks on Proceed to checkout button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "User clicks on Proceed to checkout button on Summary tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User clicks on Proceed to checkout button on Address tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "User accepts the Terms and Condition",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "User clicks on Proceed to checkout button on Shipping tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "User choose Pay by bank wire payment option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "User confirms the order",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "Validate order has been done",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "User clicks on back to order history",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefition.Open_browser()"
+});
+formatter.result({
+  "duration": 30993412585,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_click_on_Sign_In_link()"
+});
+formatter.result({
+  "duration": 4757820677,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "automationpractice25@gmail.com",
+      "offset": 21
+    },
+    {
+      "val": "test@1234",
+      "offset": 67
+    }
+  ],
+  "location": "StepDefition.User_enter_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 567902738,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.Click_on_SignIn_button()"
+});
+formatter.result({
+  "duration": 3809299774,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_clicks_on_T_Shirts_tab()"
+});
+formatter.result({
+  "duration": 6854423934,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_add_t_shirt_in_Cart()"
+});
+formatter.result({
+  "duration": 5331390427,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button()"
+});
+formatter.result({
+  "duration": 11862991797,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button_on_Summary_tab()"
+});
+formatter.result({
+  "duration": 3607144148,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button_on_Address_tab()"
+});
+formatter.result({
+  "duration": 2619701064,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_accepts_the_Terms_and_Condition()"
+});
+formatter.result({
+  "duration": 138418678,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button_on_Shipping_tab()"
+});
+formatter.result({
+  "duration": 3537592178,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_choose_Pay_by_bank_wire_payment_option()"
+});
+formatter.result({
+  "duration": 1993639356,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_confirms_the_order()"
+});
+formatter.result({
+  "duration": 9945217494,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.Validate_order_has_been_done()"
+});
+formatter.result({
+  "duration": 81280180,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_clicks_on_back_to_order_history()"
+});
+formatter.result({
+  "duration": 4350341851,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1327255266,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 157904,
+  "status": "passed"
+});
+formatter.uri("Update personal information.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Update personal Information",
+  "description": "",
+  "id": "update-personal-information",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Update personal information(First Name) in my account",
+  "description": "",
+  "id": "update-personal-information;update-personal-information(first-name)-in-my-account",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@Test"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "Open browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User click on Sign In link",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User enter username \"automationpractice25@gmail.com\" and password \"test@1234\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "Click on SignIn button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User clicks on My Personal Information button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User enter \"newtestuserrrr\" in firstname input box",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "User enters \"test@1234\" in Current password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "User enters \"test@1234\" in New Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User enters \"test@1234\" in Confirmation",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "Click on Save",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "Validate message \"Your personal information has been successfully updated.\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefition.Open_browser()"
+});
+formatter.result({
+  "duration": 24016289519,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_click_on_Sign_In_link()"
+});
+formatter.result({
+  "duration": 5565993441,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "automationpractice25@gmail.com",
+      "offset": 21
+    },
+    {
+      "val": "test@1234",
+      "offset": 67
+    }
+  ],
+  "location": "StepDefition.User_enter_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 885418232,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.Click_on_SignIn_button()"
+});
+formatter.result({
+  "duration": 3451125164,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefitionUpdateProfile.User_clicks_on_My_Personal_Information_button()"
+});
+formatter.result({
+  "duration": 3455793391,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "newtestuserrrr",
+      "offset": 12
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enter_in_firstname_input_box(String)"
+});
+formatter.result({
+  "duration": 454698342,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test@1234",
+      "offset": 13
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enters_in_Current_password(String)"
+});
+formatter.result({
+  "duration": 284902974,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test@1234",
+      "offset": 13
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enters_in_New_Password(String)"
+});
+formatter.result({
+  "duration": 213453883,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test@1234",
+      "offset": 13
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enters_in_Confirmation(String)"
+});
+formatter.result({
+  "duration": 162182914,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefitionUpdateProfile.Click_on_Save()"
+});
+formatter.result({
+  "duration": 3633760534,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Your personal information has been successfully updated.",
+      "offset": 18
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.Validate_message(String)"
+});
+formatter.result({
+  "duration": 69653941,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1290291466,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 43376,
+  "status": "passed"
+});
+});
---
+++ src/test/java/com/automationpractice/stepdefinition/StepDefitionUpdateProfile.java
@@ -0,0 +1,80 @@
+package com.automationpractice.stepdefinition;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import com.automationpractice.Base.BaseClass;
+import com.automationpractice.pages.HomePage;
+import com.automationpractice.pages.LoginAndRegistration;
+import com.automationpractice.pages.MyAccount;
+import com.automationpractice.utils.ReadConfig;
+import com.automationpractice.utils.WebDriverCommonMethods;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefitionUpdateProfile {
+	public static WebDriver driver;
+	BaseClass                base                               =      new BaseClass();
+	WebDriverCommonMethods   commom                             =      new WebDriverCommonMethods();
+	HomePage                 homepage                           =      new HomePage();
+	LoginAndRegistration     login                              =      new LoginAndRegistration();
+	MyAccount                myAccount                          =      new MyAccount();
+
+
+	/*Steps starts from here*/
+		
+	@When("^User clicks on My Personal Information button$")
+	public void User_clicks_on_My_Personal_Information_button() throws Throwable {
+		myAccount.clickMyPersonalInformation();
+	}
+
+	@When("^User enter \"([^\"]*)\" in firstname input box$")
+	public void User_enter_in_firstname_input_box(String firstName) throws Throwable {
+		System.out.println("FirstName value from feature file " + firstName);
+		myAccount.enterFirstName(firstName);
+	}
+
+	@When("^Click on Save$")
+	public void Click_on_Save() throws Throwable {
+		myAccount.clickSave();
+	}
+
+	@When("^User enters \"([^\"]*)\" in Current password$")
+	public void User_enters_in_Current_password(String currentPWD) throws Throwable {
+		myAccount.enterCurrentPWD(currentPWD);
+	}
+
+	@When("^User enters \"([^\"]*)\" in New Password$")
+	public void User_enters_in_New_Password(String newPassword) throws Throwable {
+		myAccount.enterNewPWD(newPassword);
+	}
+
+	@When("^User enters \"([^\"]*)\" in Confirmation$")
+	public void User_enters_in_Confirmation(String confirmPWD) throws Throwable {
+		myAccount.enterConfirmPWD(confirmPWD);
+	}
+
+	@Then("^Validate message \"([^\"]*)\"$")
+	public void Validate_message(String expectedMessage) throws Throwable {
+		myAccount.validateUserUpdateSuccessMessage(expectedMessage);
+	}	
+
+	@After 
+	public void afterScenario(Scenario scenario) {
+
+		if (scenario.isFailed()) { // Take a screenshot... 
+			final byte[] screenshot =((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenshot, "image/png"); // ... and embed it in the report. 
+		}
+		if(this.driver!=null) { 
+			this.driver.quit(); }
+	}
+}
